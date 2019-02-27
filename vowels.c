8f4783a3bd7829378858c60611d238ca6e748d4b
monishaviji001 <47887766+monishaviji001@users.noreply.github.com>
2019-02-27 16:16:34 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -2,8 +2,9 @@
 int main()
 {
 int i;
+  char a[100];
 printf("enter a string:");
-if(a[i]=='a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U')
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
 {
 printf("vowels");
 }
