2d5faccaca88ab8b9d0e42493d8a4657848b17a2
monishaviji001 <47887766+monishaviji001@users.noreply.github.com>
2019-02-27 16:07:25 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter a string:");
+if(a[i]=='a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U')
+{
+printf("vowels");
+}
+else
+{
+printf("consonents");
+}
+return 0;
+}
