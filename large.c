b89e10976e857bf0a584016754ab618958f0dd99
monishaviji001 <47887766+monishaviji001@users.noreply.github.com>
2019-03-19 19:04:30 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int a, b, c;
+printf("enter a,b,c");
+scanf("%d%d%D",&a,&b,&c);
+if(a>b)
+{
+printf{"a is large");
+}
+else
+{
+printf("b is large");
+}
+else if(b>c}
+{
+printf("b is large");
+}
+else
+{
+printf("c is large");
+else
+{
+printf("c is large");
+}
+return 0;
+}
